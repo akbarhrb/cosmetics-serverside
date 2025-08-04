4482f3930520c0ed0eb68254316f99696c7a0d5d
Ali Harb <133803169+akbarhrb@users.noreply.github.com>
2025-08-04 16:15:35 +0300

    Update entrypoint.sh

--- cosmetics/entrypoint.sh
+++ cosmetics/entrypoint.sh
@@ -5,7 +5,7 @@ echo "Waiting for PostgreSQL to be available..."
 # Try to connect to the database by checking migration status
 until php artisan migrate:status > /dev/null 2>&1; do
   >&2 echo "PostgreSQL is unavailable - sleeping"
-  sleep 3
+  sleep 10
 done
 
 echo "PostgreSQL is up - running migrations..."
