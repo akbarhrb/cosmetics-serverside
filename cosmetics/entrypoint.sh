168f06d8477ebce927128930c1c1525609c272ba
Ali Harb <133803169+akbarhrb@users.noreply.github.com>
2025-09-21 23:45:34 +0300

    Update entrypoint.sh

--- cosmetics/entrypoint.sh
+++ cosmetics/entrypoint.sh
@@ -7,6 +7,15 @@ until php artisan migrate:status > /dev/null 2>&1; do
   >&2 echo "PostgreSQL is unavailable - sleeping"
   sleep 10
 done
+# Clear and cache config (important to run after .env is loaded)
+echo "🧹 Clearing and caching config, routes, and views..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 echo "PostgreSQL is up - running migrations..."
 php artisan migrate --force
